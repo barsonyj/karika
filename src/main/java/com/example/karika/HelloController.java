45b0db39b29166e6223312336a2dc9effe71d6b6
Bársony József <barsony@kkando.hu>
2024-02-12 19:04:18 +0100

    Add circle

--- src/main/java/com/example/karika/HelloController.java
+++ src/main/java/com/example/karika/HelloController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class HelloController {
@@ -20,22 +22,25 @@ public class HelloController {
     @FXML private Label lbRadius;
 
     private AnimationTimer timer;
-    private boolean run = false;
+    private boolean run = true;
 
+    private Circle kor = new Circle(300, 300, 200, Color.LIGHTGREY);
     private ImageView ball = new ImageView(new Image(getClass().getResourceAsStream("ball1.png")));
     private int alfa = 0;
 
     public void initialize() {
         pnJatek.setClip(new Rectangle(600, 600)); // Pane does not clip its content by default!
+        pnJatek.getChildren().add(kor);
         ball.setLayoutX(500-32);
         ball.setLayoutY(300-32);
         pnJatek.getChildren().add(ball);
         timer = new AnimationTimer() {
             @Override
-            public void handle(long l) {
+            public void handle(long now) {
                 mozgat();
             }
         };
+        timer.start();
     }
 
     @FXML private void onTimerClicked() {
@@ -48,6 +53,7 @@ public class HelloController {
 
     private void mozgat() {
         int r = (int)srRadius.getValue();
+        kor.setRadius(r);
         lbRadius.setText("Radius: " + r);
         int x = (int)(300 + r * Math.cos(alfa/180.0*3.14))-32;
         int y = (int)(300 - r * Math.sin(alfa/180.0*3.14))-32;
