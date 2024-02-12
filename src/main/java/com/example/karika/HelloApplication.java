3339087e1a25f322e950b344df4261087c26095f
Bársony József <barsony@kkando.hu>
2024-02-12 12:56:14 +0100

    Moving

--- src/main/java/com/example/karika/HelloApplication.java
+++ src/main/java/com/example/karika/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.karika;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Karika");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("ball1.png")));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/karika/HelloController.java
+++ src/main/java/com/example/karika/HelloController.java
@@ -1,14 +1,65 @@
 package com.example.karika;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML private Pane pnJatek;
+    @FXML private Button btTimer;
+
+    private AnimationTimer timer;
+    private boolean run = false;
+
+    private ImageView ball = new ImageView(new Image(getClass().getResourceAsStream("ball1.png")));
+    private int alfa = 0;
+
+    public void initialize() {
+        pnJatek.setClip(new Rectangle(600, 600)); // Pane does not clip its content by default!
+        ball.setLayoutX(500-32);
+        ball.setLayoutY(300-32);
+        pnJatek.getChildren().add(ball);
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                mozgat();
+            }
+        };
     }
+
+    @FXML private void onTimerClicked() {
+        if (run == false) {
+            timer.start(); btTimer.setText("Stop"); run = true;
+        } else {
+            timer.stop(); btTimer.setText("Start"); run = false;
+        }
+    }
+
+    private void mozgat() {
+        int r = 200;
+        int x = (int)(300 + r * Math.cos(alfa/180.0*3.14))-32;
+        int y = (int)(300 - r * Math.sin(alfa/180.0*3.14))-32;
+        ball.setLayoutX(x);
+        ball.setLayoutY(y);
+        alfa += 4;
+    }
+
+    /*
+    RotateTransition rt = new RotateTransition(Duration.millis(2000));
+        rt.setFromAngle(0);
+        rt.setToAngle(360);
+    TranslateTransition tt = new TranslateTransition(Duration.millis(2000));
+    tt.setFromY(TILE_HEIGHT * (y - rows));
+    tt.setToY(y * TILE_HEIGHT);
+    tt.play();
+    ParallelTransition pt = new ParallelTransition(img, tt, rt);
+    pt.play();
+     */
+
 }
